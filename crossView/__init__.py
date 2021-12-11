99d025ce1efcff2a81795c6881ac13bb83126311
Zixu Zhang <zixuz@princeton.edu>
2021-12-10 23:47:30 -0500

    fix import

--- crossView/__init__.py
+++ crossView/__init__.py
@@ -3,5 +3,5 @@ from .model import *
 from .CrossViewTransformer import CrossViewTransformer
 from .CycledViewProjection import CycledViewProjection
 from .full_model import PVA_model
-from .full_model import PVA_model2
+from .full_model2 import PVA_model2
 from .combine_loss import *
